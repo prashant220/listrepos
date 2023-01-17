c5e3c15563cd5f50b3acd27337e2d04bef19efc9
prashant <ppoudel624@gmail.com>
2023-01-17 14:12:57 +0545

    testing

--- src/globals/styles.js
+++ src/globals/styles.js
@@ -16,14 +16,6 @@ html {
 }
 `;
 
-export const Theme = {
-  colors: { primary: "#faf4e4", secondary: "#19886b", tertiary: "#d1e7e1" },
-  fontSizes: { title: "clamp(1.25rem, 5vw, 4rem)", body: "1rem" },
-  mediaQuerySmall: "320px",
-  mediaQueryMedium: "600px",
-  mediaQueryLarge: "800px",
-};
-
 export const DivFlexCenter = styled.div`
   display: flex;
   align-items: center;
